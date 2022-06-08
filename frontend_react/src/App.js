bd89308f161cfd5fe71cfbe3b62efc1691502919
tonykot810 <tonykot10@gmail.com>
2022-06-09 00:03:18 +0800

    frontend folder setup

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+
+import {About,Footer,Header,Skills,Testimonial,Work} from './container'
+import { NavBar } from './components'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <NavBar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonial/>
+      <Footer/>
+    </div>
   )
 }
 
