d8b623f72dd4c0132f83165af0e76d327a0f137a
tonykot810 <tonykot10@gmail.com>
2022-06-15 04:50:11 +0800

    Work upload&loading

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
+import {AiFillEye, AiFillGithub} from 'react-icons/ai'
+import { animate, motion } from 'framer-motion'
 
+
+import { AppWrap } from '../../wrapper'
+import { urlFor, client } from '../../client';
 import './Work.scss';
+import { HiAdjustments } from 'react-icons/hi';
+import { FaHandLizard } from 'react-icons/fa';
 
 const Work = () => {
+  const [activeFilter, setactiveFilter] = useState('All');
+  const [animateCard, setanimateCard] = useState({y:0, opacity: 1});
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+    client.fetch(query)
+    .then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    })
+  }, [])
+  
+
+  const handleWorkFilter = (item) => {
+
+  }
   return (
-    <div>Work</div>
+    <>
+     <h2 className="head-text">My creative <span>Profolio </span><span>section</span></h2>
+
+     <div className="app__work_filter">
+      {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        <div 
+        key={index}
+        onClick={() => handleWorkFilter(item)}
+        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+        >
+          {item}
+          </div>
+      ))}
+     </div>
+     <motion.div
+     animate={animateCard}
+     transition={{ duration: 0.5, delayChildren: 0.5 }}
+     className="app__work_portfolio"
+     >
+      {filterWork.map((work, index) => (
+        <div className="app__work-item app__flex" key={index}>
+          <div className="app__work-img app__flex">
+          <img src={urlFor(work.imgUrl)} alt={work.name} />
+          </div>
+        </div>
+      ))}
+     </motion.div>
+    </>
   )
 }
 
