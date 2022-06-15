14c3f77d0888321aad83b02e813a4f7868aac45c
tonykot810 <tonykot10@gmail.com>
2022-06-15 21:54:54 +0800

    Works ui setup

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,5 +1,5 @@
 import React , {useState, useEffect} from 'react'
-import {AiFillEye, AiFillGithub} from 'react-icons/ai'
+import {AiFillEye, AiFillGithub, AiFillGitlab} from 'react-icons/ai'
 import { animate, motion } from 'framer-motion'
 
 
@@ -32,7 +32,7 @@ const Work = () => {
     <>
      <h2 className="head-text">My creative <span>Profolio </span><span>section</span></h2>
 
-     <div className="app__work_filter">
+     <div className="app__work-filter">
       {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
         <div 
         key={index}
@@ -52,6 +52,40 @@ const Work = () => {
         <div className="app__work-item app__flex" key={index}>
           <div className="app__work-img app__flex">
           <img src={urlFor(work.imgUrl)} alt={work.name} />
+          <motion.div
+          whileHover={{opacity:[ 0,1 ]}}
+          transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+          className="app__work-hover app__flex"
+          >
+            <a href={work.projectLink} target="_blank" rel="noreferrer">
+              <motion.div
+              whileInView={{scale:[0, 1]}}
+              whileHover={{scale:[ 1, 0.9 ]}}
+              transition={{duration: 0.25}}
+              className="app__flex"
+              >
+                <AiFillEye/>
+              </motion.div>
+            </a>
+            <a href={work.codeLink} target="_blank" rel="noreferrer">
+              <motion.div
+              whileInView={{scale:[0, 1]}}
+              whileHover={{scale:[ 1, 0.9 ]}}
+              transition={{duration: 0.25}}
+              className="app__flex"
+              >
+                <AiFillGithub/>
+              </motion.div>
+            </a>
+          </motion.div>
+          </div>
+          <div className="app__work-content app__flex">
+            <h4 className="bold-text">{work.title}</h4>
+            <p className="p-text" style={{marginTop: 10}}>{work.description}</p>
+
+            <div className="app__work-tag" app__flex>
+              <p className="p-text">{work.tags[0]}</p>
+            </div>
           </div>
         </div>
       ))}
@@ -60,4 +94,4 @@ const Work = () => {
   )
 }
 
-export default Work;
+export default AppWrap(Work, 'work');
