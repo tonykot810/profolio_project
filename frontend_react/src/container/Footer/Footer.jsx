413caf5064a846afcca68194b51958213cc19bf5
tonykot810 <tonykot10@gmail.com>
2022-06-19 20:06:32 +0800

    Footer section

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
       className="p-text"
       placeholder="Your Message"
       value={message}
-      name={message}
+      name="message"
       onChange={handleChangeInput}
       />
     </div>
